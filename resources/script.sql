0bd984e31979718655a5a611709abb67786eb629
Lucas Nunes <lucas3052003@gmail.com>
2023-07-02 14:53:55 -0300

    Finalizado

--- resources/script.sql
+++
@@ -1,9 +0,0 @@
-CREATE database publico;
-
-CREATE TABLE clientes(
-    id_cliente INT NOT NULL auto_increment PRIMARY KEY,
-    nome varchar(40),
-    email varchar(50),
-    idade int,
-    dataCadastro date
-)
